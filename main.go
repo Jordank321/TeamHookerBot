f868ba84b4c1b8b9023995ad987b1168b236d3d3
Jordan Kelwick <jordankelwick@gmail.com>
2018-12-18 03:14:10 +0000

    HTTPS?

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"crypto/tls"
 	"net/http"
 
 	"github.com/spf13/viper"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	viper.AutomaticEnv()
+
+	mux := http.NewServeMux()
 	httpHandler := NewHandler(true, viper.GetString("TEAMS_WEBHOOK_KEY"), webHook{})
-	http.ListenAndServe(":80", http.HandlerFunc(httpHandler))
+	mux.HandleFunc("/", httpHandler)
+
+	certManager := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache("certs"),
+	}
+
+	server := &http.Server{
+		Addr:    ":443",
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	server.ListenAndServeTLS("", "")
 }
 
 type webHook struct {
